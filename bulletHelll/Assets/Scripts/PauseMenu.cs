d6e400747e23400283b0861290c3dbd905e50447
cryzzis <64131397+cryzzis@users.noreply.github.com>
2021-07-30 15:49:49 +0200

    main menu and pauseMenu

--- bulletHelll/Assets/Scripts/PauseMenu.cs
+++ bulletHelll/Assets/Scripts/PauseMenu.cs
@@ -1,18 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    bool isPaused = false;
+    [SerializeField] GameObject Menu;
+
+    public void Continue()
     {
-        
+        Debug.Log("Continued");
+        Time.timeScale = 1;
+        Menu.SetActive(false);
+        isPaused = false;
+    }
+    public void Options()
+    {
+
     }
 
+    public void QuitSession()
+    {
+        SceneManager.LoadScene(0);
+    }
+    void Start()
+    {
+
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            if (isPaused)
+            {
+                Continue();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+    }
+
+    private void Pause()
+    {
+        Time.timeScale = 0;
+        Menu.SetActive(true);
+        isPaused = true;
+        Debug.Log("Paused");
     }
 }
